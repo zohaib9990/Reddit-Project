5b3c9f690f23fa9bea3d015827d4e3d1ec211e1b
Muhammad Zohaib Hussain <144581164+zohaib9990@users.noreply.github.com>
2025-12-24 23:21:02 +0500

    Update backend.tf

--- Jenkins-server-TF/backend.tf
+++ Jenkins-server-TF/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "my-reddit-bucket-1"
+    bucket         = "my-reddit-bucket-16"
     region         = "us-east-1"
     key            = "End-to-End-Kubernetes-DevSecOps-Tetris-Project/Jenkins-Server-TF/terraform.tfstate"
     dynamodb_table = "Lock-Files"
