97b30d8f4689d213a3b0d56b48289955e216d064
Muhammad Zohaib Hussain <144581164+zohaib9990@users.noreply.github.com>
2025-12-24 23:58:46 +0500

    Remove security-group-name variable
    Removed unused security group variable.

--- EKS-TF/variables.tf
+++ EKS-TF/variables.tf
@@ -3,7 +3,6 @@ variable "igw-name" {}
 variable "rt-name2" {}
 variable "subnet-name" {}
 variable "subnet-name2" {}
-variable "security-group-name" {}
 variable "iam-role-eks" {}
 variable "iam-role-node" {}
 variable "iam-policy-eks" {}
