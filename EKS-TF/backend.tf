66ba0fb31ebbdbc553c8f160afe3a34dd2891140
Muhammad Zohaib Hussain <144581164+zohaib9990@users.noreply.github.com>
2025-12-24 23:34:18 +0500

    Update backend.tf

--- EKS-TF/backend.tf
+++ EKS-TF/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "tetris-bucket"
+    bucket         = "tetris-bucket-zohaib"
     region         = "us-east-1"
     key            = "EKS-DevSecOps-Tetris-Project/EKS-TF/terraform.tfstate"
     dynamodb_table = "Lock-Files"
